05e63dbb9fa32d1d4401cce8d80c33db9d67a0f9
Recrusion <entertohacking@gmail.com>
2025-08-28 13:54:46 +0300

    the models folder has been rebuilt, unnecessary structures and files have been removed

--- internal/models/indicators.go
+++ internal/models/indicators.go
@@ -1,12 +1,5 @@
 package models
 
-type Indicators struct {
-	Username  string `json:"username"`
-	Indicator string `json:"indicator"`
-	Score     int    `json:"points"`
-	Total     int    `json:"max"`
-}
-
 type Indicator struct {
 	Indicator string `json:"indicator"`
 	Score     int    `json:"points"`
