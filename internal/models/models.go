ed00abfa104155963a3859c81149ff55d5530d7a
Recrusion <entertohacking@gmail.com>
2025-08-29 13:00:21 +0300

    models folder rebuilt

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,10 @@
 package models
 
+type Users struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type Indicator struct {
 	Indicator string `json:"indicator"`
 	Score     int    `json:"points"`
