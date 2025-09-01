850e48c8b69cc32b545e3c4e3c5ae0800280644b
Recrusion <entertohacking@gmail.com>
2025-09-01 10:39:17 +0300

    added pagination in business-logic layer

--- internal/services/services.go
+++ internal/services/services.go
@@ -10,7 +10,7 @@ type Service interface {
 	Login(ctx context.Context, username, password string) error
 	AddIndicator(ctx context.Context, username, indicator string, total int) error
 	IncreaseScore(ctx context.Context, username, indicator string) error
-	GetAllIndicators(ctx context.Context, username string) ([]models.Indicator, error)
+	GetAllIndicators(ctx context.Context, username string, pageSize, page int) ([]models.Indicator, error)
 	DeleteIndicators(ctx context.Context, username, indicator string) error
 	CreateAccessToken(ctx context.Context, username string) (string, error)
 	CreateRefreshToken(ctx context.Context, username string) (string, error)
